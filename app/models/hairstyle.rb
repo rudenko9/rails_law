45490caec8c1ccfc8d9e1a39d83067e654b5b05e
github username <github email address>
2019-08-12 23:14:44 -0400

    delete some validations

--- app/models/hairstyle.rb
+++ app/models/hairstyle.rb
@@ -1,5 +1,6 @@
 class Hairstyle < ApplicationRecord
   belongs_to :user
+  belongs_to :hair_stylist
   validates :haircut, presence: true
   validates :coloring, presence: true
   validates :haircut_and_coloring, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ accepts_nested_attributes_for :appointments
 
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+        #:omniauthable, :omniauth_providers: [:github]
+
+
+
+
+
+
 
 end
