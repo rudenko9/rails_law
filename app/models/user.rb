c5f9b84002ed4cda02988e4e12e3b22d9ee251a2
github username <github email address>
2019-08-13 13:37:32 -0400

    add routes and method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ accepts_nested_attributes_for :appointments
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid 
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#show'
-  devise_for :users
+  devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
