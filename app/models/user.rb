cb5378308d08eaf74e94294fd5006de4221d714d
github username <github email address>
2019-08-12 23:53:02 -0400

    add some info in initializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,13 @@ accepts_nested_attributes_for :appointments
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_provider => [:github]
 
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
 
 
 end
