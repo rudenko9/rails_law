7a5c72800ca98bf33a97c437f2a778881b9573b5
github username <github email address>
2019-08-18 17:41:55 -0400

    add controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,54 @@
+class ReviewsController < ApplicationController
+
+  def index
+    if params[:user_id]
+      @reviews = User.find(params[:user_id]).reviews
+      @user = User.find(params[:user_id])
+    else
+      redirect_to user_review_path
+    end
+  end
+
+  def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      flash[:notice] = 'User not found.'
+      redirect_to new_user_registration_path
+    else
+      @review = Review.new(user_id: params[:user_id])
+    end
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      @user = current_user
+      redirect_to user_review_path(@user, @review)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @review = review
+    @user = current_user
+  end
+
+  def destroy
+    review.destroy
+    redirect_to root_path
+  end
+
+
+    private
+
+      def review_params
+        params.require(:review).permit(:content, :user_id, :hair_stylist_id)
+      end
+
+      def review
+        @review ||= Review.find(params[:id])
+      end
+
+
+
+end
