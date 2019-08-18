b9c674a37385a51e43184543d9bf2c92bc4b841b
github username <github email address>
2019-08-18 13:47:11 -0400

    add changes to views

--- app/models/hair_stylist.rb
+++ app/models/hair_stylist.rb
@@ -1,7 +1,7 @@
 class HairStylist < ApplicationRecord
   has_many :users, through: :appointments
-  #has_many :appointment
-  #validates :name, presence: true
+  has_many :appointment
+  validates :name, presence: true
   #validates :image, presence: true
 
 
